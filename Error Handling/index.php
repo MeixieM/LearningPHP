69e738e00badb10672e2a49dd5162adb8d40ce25
Alex Fernan <mercadoalexfernan@gmail.com>
2023-01-21 10:26:02 +0800

    First Commit

---
+++ Error Handling/index.php
@@ -0,0 +1,22 @@
+<?php
+
+// Error Handling
+
+function errorHandler(
+    int $type,
+    string $msg,
+    ?string $file = null,
+    ?int $line = null
+
+) {
+    echo $type . ": " . $msg . "in " . $file . " on line " . $line;
+
+    exit;
+
+}
+
+error_reporting(E_ALL & ~E_WARNING);
+
+set_error_handler("errorHandler", E_ALL);
+
+echo $x;
