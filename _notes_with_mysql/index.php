17a2aeea27cb35cdf992b5002d325e11a1ec4e18
Alex Fernan <mercadoalexfernan@gmail.com>
2023-02-01 23:10:36 +0800

    add settings.json

--- _notes_with_mysql/index.php
+++ _notes_with_mysql/index.php
@@ -20,6 +20,7 @@ if (isset($_GET["id"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -27,41 +28,45 @@ if (isset($_GET["id"])) {
     <title>Document</title>
     <link rel="stylesheet" href="app.css">
 </head>
+
 <body>
-<div>
-    <form class="new-note" action="save.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
-        <input type="text" name="title" placeholder="Note title" autocomplete="off"
-               value="<?php echo $currentNote['title'] ?>">
-        <textarea name="description" cols="30" rows="4"
-                  placeholder="Note Description"><?php echo $currentNote['description'] ?></textarea>
-        <button>
-            <?php if ($currentNote['id']): ?>
-                Update
-            <?php else: ?>
-                New note
-            <?php endif ?>
-        </button>
-    </form>
-    <div class="notes">
-        <?php foreach ($notes as $note): ?>
-            <div class="note">
-                <div class="title">
-                    <a href="?id=<?php echo $note['id'] ?>">
-                        <?php echo $note['title'] ?>
-                    </a>
+    <div>
+        <form class="new-note" action="save.php" method="post">
+            <input type="hidden" name="id" value="<?php echo $currentNote['id'] ?>">
+            <input type="text" name="title" placeholder="Note title" autocomplete="off"
+                value="<?php echo $currentNote['title'] ?>">
+            <textarea name="description" cols="30" rows="4"
+                placeholder="Note Description"><?php echo $currentNote['description'] ?></textarea>
+            <button>
+                <?php if ($currentNote['id']): ?>
+                    Update
+                <?php else: ?>
+                    New note
+                <?php endif ?>
+            </button>
+        </form>
+        <div class="notes">
+            <?php foreach ($notes as $note): ?>
+                <div class="note">
+                    <div class="title">
+                        <a href="?id=<?php echo $note['id'] ?>">
+                            <?php echo $note['title'] ?>
+                        </a>
+                    </div>
+                    <div class="description">
+                        <?php echo $note['description'] ?>
+                    </div>
+                    <small>
+                        <?php echo date('d/m/Y H:i', strtotime($note['create_date'])) ?>
+                    </small>
+                    <form action="delete.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $note['id'] ?>">
+                        <button class="close">X</button>
+                    </form>
                 </div>
-                <div class="description">
-                    <?php echo $note['description'] ?>
-                </div>
-                <small><?php echo date('d/m/Y H:i', strtotime($note['create_date'])) ?></small>
-                <form action="delete.php" method="post">
-                    <input type="hidden" name="id" value="<?php echo $note['id'] ?>">
-                    <button class="close">X</button>
-                </form>
-            </div>
-        <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
     </div>
-</div>
 </body>
+
 </html>
