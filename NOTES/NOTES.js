6c9e8e270ca7d3c666078cbf08a7de5010db9498
Alex Fernan <mercadoalexfernan@gmail.com>
2023-02-06 02:29:22 +0800

    NEW NOTES

--- NOTES/NOTES.js
+++ NOTES/NOTES.js
@@ -132,3 +132,64 @@ $(document).ready(function() {
 </div>
 
 
+function updateProduct() {
+    var productId = $('#productId').val();
+    var productName = $('#productName').val();
+    var productDescription = $('#productDescription').val();
+    var productPrice = $('#productPrice').val();
+    
+    $.ajax({
+      url: 'update_product.php',
+      type: 'post',
+      data: {productId: productId, productName: productName, productDescription: productDescription, productPrice: productPrice},
+      success: function(response) {
+        // update the product in the table or do something else
+        // close the modal
+        $('#updateModal').modal('hide');
+      }
+    });
+  }
+  
+
+
+  <?php
+// database connection details
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inventory";
+
+// create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+// check if the form was submitted
+if (isset($_POST["productId"]) && isset($_POST["productName"]) && isset($_POST["productDescription"]) && isset($_POST["productPrice"])) {
+  $productId = $_POST["productId"];
+  $productName = $_POST["productName"];
+  $productDescription = $_POST["productDescription"];
+  $productPrice = $_POST["productPrice"];
+  
+  // check if an image was uploaded
+  if (isset($_FILES["productImage"]) && $_FILES["productImage"]["error"] == 0) {
+    $image = addslashes(file_get_contents($_FILES["productImage"]["tmp_name"]));
+    $sql = "UPDATE products SET name='$productName', description='$productDescription', price=$productPrice, image='$image' WHERE id=$productId";
+  } else {
+    $sql = "UPDATE products SET name='$productName', description='$productDescription', price=$productPrice WHERE id=$productId";
+  }
+  
+  // update the product in the database
+  if (mysqli_query($conn, $sql)) {
+    echo "Product updated successfully";
+  } else {
+    echo "Error updating product: " . mysqli_error($conn);
+  }
+}
+
+// close the connection
+mysqli_close($conn);
+?>
